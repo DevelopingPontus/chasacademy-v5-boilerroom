aa0e8b077186363ec15228a6bee7232eaa8ffa49
Oliver Öhrström <xzereha@live.se>
2025-12-09 09:46:14 +0100

    Member create function

--- src/main/java/se/chasacademy/databaser/v5/boilerroom/Repositories/MemberRepository.java
+++ src/main/java/se/chasacademy/databaser/v5/boilerroom/Repositories/MemberRepository.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 @Repository
 public class MemberRepository {
     private JdbcClient jdbcClient;
+
     public MemberRepository(JdbcClient jdbcClient) {
         this.jdbcClient = jdbcClient;
     }
@@ -30,5 +31,29 @@ public class MemberRepository {
         }
     }
 
+    /**
+     * Create a new member in the database
+     * 
+     * @param name    The name of the member
+     * @param address The address of the member
+     * @param email   The email of the member
+     * @throws IllegalArgumentException if any of the parameters are blank or null
+     * @return true if the member was created, false otherwise
+     */
+    public boolean create(@NonNull String name, @NonNull String address, @NonNull String email) {
+        if (name == null || address == null || email == null) {
+            throw new IllegalArgumentException("Name, email and address cannot be null");
+        }
+        if (name.isBlank() || address.isBlank() || email.isBlank()) {
+            throw new IllegalArgumentException("Name, email and address cannot be blank");
+        }
+
+        int rowsAffected = jdbcClient.sql("insert into members (name, address, email) values (?, ?, ?)")
+                .param(name)
+                .param(address)
+                .param(email)
+                .update();
+        return rowsAffected == 1;
+    }
 
 }
